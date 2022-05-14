dd06df5a8d1bd11e92ad1ed986dad71b04f3f3e6
JosephGu <285822850@qq.com>
2022-05-14 10:54:14 +0800

    add tailwind

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <Clock></Clock>
+      <Timer></Timer>
     </div>
   );
 }
---
+++ src/components/Timer.js
@@ -0,0 +1,14 @@
+const Timer = () => {
+
+    return (
+        <div className="p-6">
+            <div>
+                <input placeholder="Hour"></input>
+                <input placeholder="Minute"></input>
+                <input placeholder="Second"></input>
+            </div>
+        </div>
+    )
+}
+
+export default Timer;
